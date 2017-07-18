9e454c303d80ef9b3d45a6b8fac921abf17559b7
Matthew Healy <matthew.healy@soundcloud.com>
2017-07-18 10:22:02 +0200

    First commit

---
+++ PianoRoll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PianoRoll
+//
+//  Created by Matthew Healy on 18.07.17.
+//  Copyright © 2017 Matthew Healy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
