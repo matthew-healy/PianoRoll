813cac19bb3d4dc569332f5e489d9cab7ba7bfe2
Matthew Healy <matthew.healy@soundcloud.com>
2017-07-24 16:07:48 +0200

    Make PianoRollCoordinate equatable

--- PianoRollLib/PianoRollCoordinate.swift
+++ PianoRollLib/PianoRollCoordinate.swift
@@ -1,4 +1,10 @@
-struct PianoRollCoordinate {
+struct PianoRollCoordinate: Equatable {
     let pitch: Int
     let time: Int
+
+    static func ==(lhs: PianoRollCoordinate, rhs: PianoRollCoordinate) -> Bool {
+        return lhs.pitch == rhs.pitch
+            && lhs.time == rhs.time
+
+    }
 }
--- PianoRollLibTests/PianoRollCoordinateTests.swift
+++ PianoRollLibTests/PianoRollCoordinateTests.swift
@@ -5,6 +5,8 @@ class PianoRollCoordinateTests: XCTestCase {
     
     var subject: PianoRollCoordinate!
 
+    // MARK: property tests
+
     func test_canGetPitch() {
         subject = .create()
         Assert.symmetricEquality(0, subject.pitch)
@@ -23,6 +25,25 @@ class PianoRollCoordinateTests: XCTestCase {
     func test_timeIsNotHardcoded() {
         subject = .create(time: 4)
         XCTAssertEqual(4, subject.time)
+    }
+
+    // MARK: equals tests
+
+    var lhs, rhs: PianoRollCoordinate!
+
+    func test_equals_propertiesMatch_true() {
+        (lhs, rhs) = (.create(), .create())
+        Assert.symmetricEquality(lhs, rhs)
+    }
+
+    func test_equals_pitchesDiffer_false() {
+        (lhs, rhs) = (.create(), .create(pitch: 5))
+        Assert.symmetricNonEquality(lhs, rhs)
+    }
+
+    func test_equals_timesDiffer_false() {
+        (lhs, rhs) = (.create(), .create(time: 3))
+        Assert.symmetricNonEquality(lhs, rhs)
     }
 
 }
